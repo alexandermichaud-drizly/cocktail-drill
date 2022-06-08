4fedd18456c90f14fa052a936a4adc7b16593668
ARM <alexander.michaud@aya.yale.edu>
2022-06-08 16:21:16 -0400

    Top level await

--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,15 @@ type Recipe = {
   servedIn: VesselsType;
 }
 
-const firstQuestion = async () => {
-  const answer = await inquirer.prompt({
-    name: "test_prompt",
-    type: "list",
-    message: "Choose from the list",
-    choices: [
-      "Option 1",
-      "Option 2",
-      "Option 3",
-    ]
-  });
+const answer = await inquirer.prompt({
+  name: "test_prompt",
+  type: "list",
+  message: "Choose from the list",
+  choices: [
+    "Option 1",
+    "Option 2",
+    "Option 3",
+  ]
+});
 
-  console.log(`Your answer was ${answer.test_prompt}`);
-}
-
-firstQuestion();
+console.log(`Your answer was ${answer.test_prompt}`);
