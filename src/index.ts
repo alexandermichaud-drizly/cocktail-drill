321e3db52fd346b3030c08508c0b04f5143a5096
ARM <alexander.michaud@aya.yale.edu>
2022-06-08 21:55:18 -0400

    Rename prompt

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import inquirer from 'inquirer';
 import { GameModes } from './constants';
 // import { RecipeType } from './types';
 
-let answer = await inquirer.prompt({
+const userChoice = await inquirer.prompt({
   name: "gameMode",
   type: "list",
   message: "Choose how you'd like to practice",
   choices: Object.values(GameModes),
 });
 
-console.log(`Your answer was ${answer.gameMode}`);
+console.log(`Your answer was ${userChoice.gameMode}`);
